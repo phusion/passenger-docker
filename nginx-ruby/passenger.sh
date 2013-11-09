7204ae87431d28dcd1c648cfd029fe72bec605ad
Hongli Lai (Phusion) <hongli@phusion.nl>
2013-11-09 21:20:01 +0100

    Work in progress

--- nginx-ruby/passenger.sh
+++ nginx-ruby/passenger.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -ex
 
+## Install Phusion Passenger.
 if [[ "$PASSENGER_ENTERPRISE" ]]; then
-	apt-get install -y passenger-enterprise
+	apt-get install -y nginx-extras passenger-enterprise
 else
-	apt-get install -y passenger
+	apt-get install -y nginx-extras passenger
 fi
+cp /build/nginx.conf /etc/nginx/nginx.conf
+
+## Install Nginx runit service.
+mkdir /etc/service/nginx
+cp /build/runit/nginx /etc/service/nginx/run
--- nginx-ruby/prepare.sh
+++ nginx-ruby/prepare.sh
@@ -7,6 +7,6 @@ dpkg-divert --local --rename --add /sbin/initctl
 ln -s /bin/true /sbin/initctl
 
 ## Create a user for the web app.
-addgroup --gid 1000 app
-adduser --uid 1000 --gid 1000 --disabled-password --gecos "Application" app
+addgroup --gid 9999 app
+adduser --uid 9999 --gid 9999 --disabled-password --gecos "Application" app
 usermod -L app
