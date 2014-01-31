17e7d09ba7ee7d35518a4b2580a51158d15a907e
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-01-31 19:56:21 +0100

    Correctly fix Bundler and Rake shebang lines.
    Closes GH-6.

--- image/ruby-finalize.sh
+++ image/ruby-finalize.sh
@@ -10,7 +10,8 @@ echo "gem: --no-ri --no-rdoc" > /etc/gemrc
 
 ## Fix shebang lines in rake and bundler so that they're run with the currently
 ## configured default Ruby instead of the Ruby they're installed with.
-sed -i 's|/usr/bin/env ruby.*$|/usr/bin/env ruby|' /usr/local/bin/rake /usr/local/bin/bundle
+sed -i 's|/usr/bin/env ruby.*$|/usr/bin/env ruby|; s|/usr/bin/ruby.*$|/usr/bin/env ruby|' \
+	/usr/local/bin/rake /usr/local/bin/bundle
 
 ## The Rails asset compiler requires a Javascript runtime.
 $minimal_apt_get_install nodejs
