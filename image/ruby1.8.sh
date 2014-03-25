76e44e1d1a853bdca257ba7e1f8b003c0370b799
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-03-25 15:57:48 +0100

    On Ruby 1.8, install Rake 10.1.1 because that's the last version supporting Ruby 1.8

--- image/ruby1.8.sh
+++ image/ruby1.8.sh
@@ -4,5 +4,6 @@ source /build/buildconfig
 set -x
 
 $minimal_apt_get_install ruby1.8 ruby1.8-dev rubygems
-gem1.8 install rake bundler --no-rdoc --no-ri
+gem1.8 install rake --no-rdoc --no-ri -v 10.1.1 # last version supporting Ruby 1.8
+gem1.8 install bundler --no-rdoc --no-ri
 /build/ruby-finalize.sh
