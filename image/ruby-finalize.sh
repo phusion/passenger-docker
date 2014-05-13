f7b8bf77318c8b1bb391ad644c4f2d94de3dc437
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-05-13 12:44:52 +0200

    The shebang for /usr/local/bin/bundler should also be changed

--- image/ruby-finalize.sh
+++ image/ruby-finalize.sh
@@ -11,7 +11,7 @@ echo "gem: --no-ri --no-rdoc" > /etc/gemrc
 ## Fix shebang lines in rake and bundler so that they're run with the currently
 ## configured default Ruby instead of the Ruby they're installed with.
 sed -i 's|/usr/bin/env ruby.*$|/usr/bin/env ruby|; s|/usr/bin/ruby.*$|/usr/bin/env ruby|' \
-	/usr/local/bin/rake /usr/local/bin/bundle
+	/usr/local/bin/rake /usr/local/bin/bundle /usr/local/bin/bundler
 
 ## The Rails asset compiler requires a Javascript runtime.
 $minimal_apt_get_install nodejs
