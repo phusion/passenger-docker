d898e85cf496fc72205a20f9b054ebfa9db80b44
Aris Pikeas <pikeas@gmail.com>
2014-08-10 21:19:05 -0700

    Remove redundant apt-get commands

--- image/enable_repos.sh
+++ image/enable_repos.sh
@@ -3,11 +3,6 @@ set -e
 source /build/buildconfig
 set -x
 
-# Temporary workaround for https://github.com/phusion/baseimage-docker/issues/73
-# TODO: remove this when baseimage-docker 0.9.11 is released
-apt-get update
-apt-get install -y ca-certificates
-
 ## Brightbox Ruby 1.9.3, 2.0 and 2.1
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C3173AA6
 echo deb http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu trusty main > /etc/apt/sources.list.d/brightbox.list
