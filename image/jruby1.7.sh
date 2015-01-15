60db3e79bb11c1037584e478a88d90931c112b28
Per Lundberg <per.lundberg@ecraft.com>
2015-01-15 11:05:20 +0100

    Attempted workaround for bug in the Debian jre-8 package.

--- image/jruby1.7.sh
+++ image/jruby1.7.sh
@@ -4,6 +4,7 @@ source /build/buildconfig
 set -x
 
 apt-get install -y -t sid openjdk-8-jre-headless
+dpkg-reconfigure ca-certificates-java
 
 curl https://s3.amazonaws.com/jruby.org/downloads/1.7.18/jruby-bin-1.7.18.tar.gz -o /tmp/jruby-bin-1.7.18.tar.gz
 cd /usr/local
