849feb191d7368405eca042eb1899678acb6785e
Luiz Gustavo Nogara <nogara@edumobi.com.br>
2016-11-30 14:45:39 -0200

    Upgrades ruby 2.3 to 2.3.3

--- image/install.sh
+++ image/install.sh
@@ -9,7 +9,7 @@ run /pd_build/utilities.sh
 if [[ "$ruby20" = 1 ]]; then run /pd_build/ruby-2.0.0.sh; fi
 if [[ "$ruby21" = 1 ]]; then run /pd_build/ruby-2.1.9.sh; fi
 if [[ "$ruby22" = 1 ]]; then run /pd_build/ruby-2.2.5.sh; fi
-if [[ "$ruby23" = 1 ]]; then run /pd_build/ruby-2.3.1.sh; fi
+if [[ "$ruby23" = 1 ]]; then run /pd_build/ruby-2.3.3.sh; fi
 if [[ "$jruby91" = 1 ]]; then run /pd_build/jruby-9.1.2.0.sh; fi
 if [[ "$python" = 1 ]]; then run /pd_build/python.sh; fi
 if [[ "$nodejs" = 1 ]]; then run /pd_build/nodejs.sh; fi
