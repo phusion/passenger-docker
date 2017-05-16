69ab1b53e0b541aacf2ef794eec46eba5411dd1b
Camden Narzt <c.narzt@me.com>
2017-05-16 07:45:31 -0600

    Fix finalize bug

--- image/finalize.sh
+++ image/finalize.sh
@@ -9,7 +9,7 @@ if [[ -e /usr/local/rvm ]]; then
 fi
 
 run apt-get remove -y autoconf automake
-run apt-get autoremove
+run apt-get autoremove -y
 run apt-get clean
 run rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 if [[ "$final" = 1 ]]; then
