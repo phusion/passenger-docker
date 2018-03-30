04703974aa2ee98addf83fd69335f16774e0113e
Georg Ledermann <mail@georg-ledermann.de>
2018-03-30 08:09:07 +0200

    Upgrade Ruby to 2.5.1 / 2.4.4 / 2.3.7 / 2.2.10

--- image/install.sh
+++ image/install.sh
@@ -8,10 +8,10 @@ run /pd_build/utilities.sh
 
 if [[ "$ruby20" = 1 ]]; then run /pd_build/ruby-2.0.0.sh; fi
 if [[ "$ruby21" = 1 ]]; then run /pd_build/ruby-2.1.9.sh; fi
-if [[ "$ruby22" = 1 ]]; then run /pd_build/ruby-2.2.9.sh; fi
-if [[ "$ruby23" = 1 ]]; then run /pd_build/ruby-2.3.6.sh; fi
-if [[ "$ruby24" = 1 ]]; then run /pd_build/ruby-2.4.3.sh; fi
-if [[ "$ruby25" = 1 ]]; then run /pd_build/ruby-2.5.0.sh; fi
+if [[ "$ruby22" = 1 ]]; then run /pd_build/ruby-2.2.10.sh; fi
+if [[ "$ruby23" = 1 ]]; then run /pd_build/ruby-2.3.7.sh; fi
+if [[ "$ruby24" = 1 ]]; then run /pd_build/ruby-2.4.4.sh; fi
+if [[ "$ruby25" = 1 ]]; then run /pd_build/ruby-2.5.1.sh; fi
 if [[ "$jruby91" = 1 ]]; then run /pd_build/jruby-9.1.2.0.sh; fi
 if [[ "$python" = 1 ]]; then run /pd_build/python.sh; fi
 if [[ "$nodejs" = 1 ]]; then run /pd_build/nodejs.sh; fi
