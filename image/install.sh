704bdaaf60b241d97b0dc52e1b31cf636dd29a96
Alan Hodgson <ahodgson@simkin.ca>
2022-12-25 15:09:06 -0800

    Add a Ruby 3.2.0 image

--- image/install.sh
+++ image/install.sh
@@ -10,6 +10,7 @@ run /pd_build/utilities.sh
 if [[ "$ruby27" = 1 ]]; then run /pd_build/ruby-2.7.*.sh; fi
 if [[ "$ruby30" = 1 ]]; then run /pd_build/ruby-3.0.*.sh; fi
 if [[ "$ruby31" = 1 ]]; then run /pd_build/ruby-3.1.*.sh; fi
+if [[ "$ruby32" = 1 ]]; then run /pd_build/ruby-3.2.*.sh; fi
 if [[ "$jruby93" = 1 ]]; then run /pd_build/jruby-9.3.*.sh; fi
 if [[ "$python" = 1 ]]; then run /pd_build/python.sh; fi
 if [[ "$nodejs" = 1 ]]; then run /pd_build/nodejs.sh; fi
