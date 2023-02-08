f0ad742bd3be3c5f491785e637cf666cd286299a
David Jones <dgjones@gmail.com>
2023-02-08 12:23:26 -0800

    Remove rustc after ruby has been compiled with YJIT

--- image/ruby-3.2.0.sh
+++ image/ruby-3.2.0.sh
@@ -11,6 +11,10 @@ run /pd_build/ruby_support/prepare.sh
 run minimal_apt_get_install rustc
 
 run /usr/local/rvm/bin/rvm install $RVM_ID --disable-binary || ( cat /usr/local/rvm/log/*${RVM_ID}*/*.log && false )
+
+# Remove rustc after Ruby has compiled with YJIT
+run apt-get remove -y rustc
+
 run /usr/local/rvm/bin/rvm-exec $RVM_ID@global gem install $DEFAULT_RUBY_GEMS --no-document
 # Make passenger_system_ruby work.
 run create_rvm_wrapper_script ruby3.2 $RVM_ID ruby
