21521f0b09e73ca4b7831a2b307056918cfbc4c5
Alan Hodgson <ahodgson@simkin.ca>
2023-10-12 09:41:11 -0700

    Install OpenSSL 1.1 manually for Ruby 3.0 support. This is only needed until Ruby 3.0 reaches EOL in March 2024

---
+++ image/install_openssl1.1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+if [[ ! -d /usr/local/ssl ]]; then
+	PWD=$(pwd)
+	cd /usr/local/src/
+	git clone https://github.com/openssl/openssl.git -b OpenSSL_1_1_1-stable openssl-1.1.1m
+	cd openssl-1.1.1m
+	./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib
+	make -j$(nproc)
+	make install_sw
+	ln -s /etc/ssl/certs /usr/local/ssl/certs
+
+	cd "${PWD}"
+	rm -rf /usr/local/src/openssl*
+fi
--- image/ruby-3.0.6.sh
+++ image/ruby-3.0.6.sh
@@ -2,17 +2,32 @@
 set -e
 source /pd_build/buildconfig
 
+
 RVM_ID=$(basename "$0" | sed 's/.sh$//')
 
-header "Installing $RVM_ID"
-
 run mkdir -p "/rvm_cache/${ARCH}"
+
+# Ruby 3.0 does not support OpenSSL 3.0, so we need to install 1.1 ourselves
+header "Installing OpenSSL 1.1"
+run minimal_apt_get_install zlib1g-dev
+if [[ -f /rvm_cache/${ARCH}/openssl1.1.tar.gz ]]; then
+	cd /usr/local
+	tar -xvf /rvm_cache/${ARCH}/openssl1.1.tar.gz
+	cd
+else
+	run /pd_build/install_openssl1.1.sh
+	cd /usr/local
+	tar -czvf /rvm_cache/${ARCH}/openssl1.1.tar.gz ssl
+	cd 
+fi
+
+header "Installing $RVM_ID"
 if [[ -e "/rvm_cache/${ARCH}/${RVM_ID}.tar.bz2" ]]; then
 	# use cached ruby if present
 	run /usr/local/rvm/bin/rvm mount "/rvm_cache/${ARCH}/${RVM_ID}.tar.bz2"
 else
 	# otherwise build one
-	MAKEFLAGS=-j$(nproc) run /usr/local/rvm/bin/rvm install $RVM_ID --disable-cache || ( cat /usr/local/rvm/log/*${RVM_ID}*/*.log && false )
+	MAKEFLAGS=-j$(nproc) run /usr/local/rvm/bin/rvm install $RVM_ID --with-openssl-dir=/usr/local/ssl --disable-cache || ( cat /usr/local/rvm/log/*${RVM_ID}*/*.log && false )
 	run cd "/rvm_cache/${ARCH}"
 	run /usr/local/rvm/bin/rvm prepare "${RVM_ID}"
 	run cd /
