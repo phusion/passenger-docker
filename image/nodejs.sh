b37d17fbc47bee97794c77ca9214613121e6d876
Paul Hurst <git@hursty.co.uk>
2023-10-24 12:15:01 +0100

    Simplify package installs

--- image/nodejs.sh
+++ image/nodejs.sh
@@ -5,12 +5,10 @@ set -x
 
 echo "+ Enabling Node Source APT repo"
 NODE_MAJOR=18
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt-get update
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_${NODE_MAJOR}.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
 
 ## Install Node.js (also needed for Rails asset compilation)
 minimal_apt_get_install nodejs
